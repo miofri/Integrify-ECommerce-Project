e84ad4b968ed42d8444014a65a5e2c3519bc8e43
Selvi <slvkesuma@gmail.com>
2023-05-17 21:49:41 +0300

    made object sorting work, cha cha cha

--- src/components/products/AllProducts.tsx
+++ src/components/products/AllProducts.tsx
@@ -5,6 +5,7 @@ import { productSlice } from "../../store/productSlice";
 import { createSelector } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { AllProductsInterface } from "../../interface/productsInterface";
+import { Box, Button } from "@mui/material";
 
 export const AllProducts = () => {
   const selectProductState = (state: RootState) => state.product;
@@ -12,6 +13,9 @@ export const AllProducts = () => {
     selectProductState,
     (product) => product
   );
+  const productFromStore = useSelector(selectProduct).products;
+  const [products, setProducts] =
+    useState<AllProductsInterface[]>(productFromStore);
 
   useEffect(() => {
     const waitProducts = async () => {
@@ -23,13 +27,29 @@ export const AllProducts = () => {
     waitProducts();
   }, []);
 
-  const productFromStore = useSelector(selectProduct).products;
+  useEffect(() => {
+    setProducts(productFromStore);
+  }, [productFromStore]);
+
+  const handleSort = () => {
+    const sorted = [...products].sort((a, b) => a.title.localeCompare(b.title));
+    setProducts(sorted);
+  };
 
   return (
-    <div>
-      {productFromStore.map((data) => (
-        <li style={{ color: "white" }}>{data.title}</li>
-      ))}
-    </div>
+    <>
+      <Box>
+        <Button onClick={() => handleSort()} variant="contained">
+          Sort by name
+        </Button>
+      </Box>
+      <div>
+        {products.map((data) => (
+          <li style={{ color: "white" }}>{data.title}</li>
+        ))}
+      </div>
+    </>
   );
 };
+
+// localeCompare(): string comparison that's sensitive to language-specific rules and settings, such as alphabetical order, diacritic marks, and case sensitivity.
